8c11a66463acdf73998f95c2986618f6a64b350c
joehenrickson <joehenrickson@gmail.com>
2022-09-03 00:33:21 -0500

    thought and reaction schema completed.

---
+++ models/thought.js
@@ -0,0 +1,67 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const reactionsSchema = new Schema(
+  {
+    // Set custom ID
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format('MM DD, YYYY [at] hh:mm a'),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format('MM DD, YYYY [at] hh:mm a'),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const thoughts = model('thoughts', thoughtSchema);
+
+module.exports = thoughts;
